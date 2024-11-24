16be70b2224d82ac378a0bdb77abbe78009723d9
SanAJC <146601230+SanAJC@users.noreply.github.com>
2024-11-24 15:58:42 -0500

    CRSF

--- hotel/settings.py
+++ hotel/settings.py
@@ -206,3 +206,5 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['http://*','https://web-production-7dde2.up.railway.app']
